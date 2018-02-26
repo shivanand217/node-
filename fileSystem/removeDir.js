ea5234ba8fe3ee0b249ac842d9fdf8441e7a67b1
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-02-26 18:25:40 +0530

    26-02-2018

--- fileSystem/removeDir.js
+++ fileSystem/removeDir.js
@@ -1,2 +1,22 @@
 // fs.rmdir(path, callback)
 
+// remove a directory anywhere from node console
+
+var fs = require("fs");
+
+console.log("Going to delete directory C:/tmp/test");
+fs.rmdir('/tmp/test',function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        // checking if delete happened
+        console.log("Going to read directory /tmp");
+        fs.readdir('/tmp', function(err, files) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log("The directory C:/tmp contains: "+files);
+            }
+        });
+    }
+});
