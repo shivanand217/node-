20661f7b8645d18775d57cb5ed42bb18d7ca05ea
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-02-26 18:27:17 +0530

    26-02-2018

--- fileSystem/removeDir.js
+++ fileSystem/removeDir.js
@@ -9,7 +9,7 @@ fs.rmdir('/tmp/test',function(err) {
     if(err) {
         console.log(err);
     } else {
-        // checking if delete happened
+        // Checking if delete happened
         console.log("Going to read directory /tmp");
         fs.readdir('/tmp', function(err, files) {
             if(err) {
