2b5916ca0d021ef076718bee266c785c5d44693f
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-02-26 17:07:58 +0530

    26-02-2018

--- fileSystem/makeDir.js
+++ fileSystem/makeDir.js
@@ -1,1 +1,29 @@
 // fs.mkdir(path[, mode], callback)
+
+var fs = require("fs");
+
+console.log("Going to create directory C:/tmp/test");
+fs.mkdir('/tmp/test', function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("directory C:/tmp/test created..");
+    }
+});
+
+fs.mkdir('/tmp/test1', function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("directory C:/tmp/test1 created..");
+    }
+});
+
+fs.mkdir('/tmp/test2', function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("directory C:/tmp/test2 created..");
+    }
+});
+
